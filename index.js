752f88c436c613aaab5db17aaceba4ff7fd0d47c
edhuas <gs1hdurbahn@hotmail.com>
2024-05-14 14:13:24 +0700

    hello commit

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const shellSort = (arr) => {
-  const n = arr.length;
-  for (let gap = Math.floor(n / 2); gap > 0; gap = Math.floor(gap / 2)) {
-    for (let i = gap; i < n; i++) {
-      const temp = arr[i];
-      let j;
-      for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
-        arr[j] = arr[j - gap];
-      }
-      arr[j] = temp;
+function jump(nums) {
+  let jumps = 0;
+  let currentEnd = 0;
+  let farthest = 0;
+  for (let i = 0; i < nums.length - 1; i++) {
+    farthest = Math.max(farthest, i + nums[i]);
+    if (i === currentEnd) {
+      jumps++;
+      currentEnd = farthest;
     }
   }
-  return arr;
-};
+  return jumps;
+}
